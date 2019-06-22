cd098539587d725a38faea332ca2727ea75df55d
Christian Rodriguez <christianarg@outlook.com>
2019-06-22 18:27:18 +0200

    debug mode

--- WebApCoreReact/Controllers/HomeController.cs
+++ WebApCoreReact/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using WebApCoreReact.Models;
 
@@ -10,8 +11,18 @@ namespace WebApCoreReact.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IHostingEnvironment hostingEnv;
+
+        public HomeController(IHostingEnvironment hostingEnv)
+        {
+            this.hostingEnv = hostingEnv;
+        }
         public IActionResult Index()
         {
+            if (hostingEnv.IsDevelopment())
+            {
+                return File("app/index.debug.html", "text/html");
+            }
             return File("app/index.html","text/html");
         }
 
